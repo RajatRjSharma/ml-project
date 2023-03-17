76866dcd7184956f38aa3faa47e00b0508a56aee
rajatrjsharma <merajatsharma@outlook.com>
2023-03-17 11:44:15 +0530

    EDA and MODEL Training

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logger
+# from src.logger import logging
 
 def error_message_detail(error,error_detail:sys) :
     _,_,exc_tb = error_detail.exc_info()
@@ -25,5 +25,5 @@ class CustomException(Exception):
 #     try:
 #         a=1/0
 #     except Exception as e:
-#         logger.logging.info("Divide by zero error")
+#         logging.info("Divide by zero error")
 #         raise CustomException(e,sys)
